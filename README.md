519e0d0b0c0cfd3873ab29ff5b0bbfa355ea5286
Joseph Iannone <joseph.m.iannone@gmail.com>
2020-07-22 14:38:32 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+States:
+1. Menu: [Press "l" to play as left paddle and "r" to play as right paddle]
+2. [Press "Click with mouse to begin" "Use mouse to control paddle"]
+3. Game play
+4. Max score reached
+5. Back to 1
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,13 +10,18 @@
  */
 
 import game.Game;
+import javafx.scene.control.Label;
 import javafx.geometry.Insets;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+
 
 
 public class Main extends Application {
@@ -29,9 +34,37 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.root = new VBox();
+        
         root.setAlignment(Pos.CENTER);
         root.setSpacing(10);
 	root.setPadding(new Insets(10, 10, 10, 10));
+        
+        HBox settings = new HBox();
+        settings.setSpacing(20);
+        settings.setAlignment(Pos.CENTER);
+        
+        final CheckBox rightMouseControl = new CheckBox();
+	rightMouseControl.setGraphic(new Label("Right Paddle"));
+	final CheckBox leftMouseControl = new CheckBox();
+	leftMouseControl.setGraphic(new Label("Left Paddle"));
+        
+        leftMouseControl.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                if (newValue) {
+                    rightMouseControl.setSelected(false);
+                } 
+            }
+	});
+			
+	rightMouseControl.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                if (newValue) {
+                    leftMouseControl.setSelected(false);
+                } 
+            }
+	});
         
         Scene scene = new Scene(root, 1000, 800);
         //scene.getStylesheets().add(Styles.getStylesheet());
